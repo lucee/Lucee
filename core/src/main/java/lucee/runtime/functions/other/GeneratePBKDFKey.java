6ec95ef8aef11b5f9f7e1c3e04ab6dcd647bf8ae
michaeloffner <michael@rasia.ch>
2015-10-14 19:39:12 +0200

    fixes https://luceeserver.atlassian.net/browse/LDEV-256

---
+++ core/src/main/java/lucee/runtime/functions/other/GeneratePBKDFKey.java
@@ -0,0 +1,103 @@
+/**
+ *
+ * Copyright (c) 2015, Lucee Assosication Switzerland
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+package lucee.runtime.functions.other;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import lucee.print;
+import lucee.commons.lang.StringUtil;
+import lucee.runtime.PageContext;
+import lucee.runtime.coder.Base64Coder;
+import lucee.runtime.crypt.CFMXCompat;
+import lucee.runtime.exp.FunctionException;
+import lucee.runtime.exp.PageException;
+import lucee.runtime.ext.function.BIF;
+import lucee.runtime.op.Caster;
+
+public class GeneratePBKDFKey extends BIF {
+
+	private static final long serialVersionUID = -2558116913822203235L;
+
+	public static String call(PageContext pc,String algorithm, String passPhrase, String salt) throws PageException {
+		return call(pc, algorithm, passPhrase, salt, 4096, 128);
+	}
+	
+	public static String call(PageContext pc,String algorithm, String passPhrase, String salt, double iterations) throws PageException {
+		return call(pc, algorithm, passPhrase, salt, iterations, 128);
+	}
+	public static String call(PageContext pc,String algorithm, String passPhrase, String salt, double iterations, double keySize) throws PageException {
+		// algo
+		if(StringUtil.isEmpty(algorithm))
+			throw new FunctionException(pc, "GeneratePBKDFKey", 1, "algorithm", "algorithm argument is empty.");
+		algorithm = algorithm.trim();
+		if(!StringUtil.startsWithIgnoreCase(algorithm, "PBK"))
+			throw new FunctionException(pc, "GeneratePBKDFKey", 1, "algorithm", "algorithm ["+algorithm+"] is not supported.");
+
+		
+		// TODO add provider to support addional keys by addin a provider that is supporting it 
+		SecretKeyFactory key=null;
+		try {
+			key = SecretKeyFactory.getInstance(algorithm);
+		} catch (NoSuchAlgorithmException e) {
+			if(!algorithm.equalsIgnoreCase("PBKDF2WithHmacSHA1"))
+				throw new FunctionException(pc, "GeneratePBKDFKey", 1, "algorithm", "only the algorithm [PBKDF2WithHmacSHA1] is supported by the build in .");
+			else 
+				throw Caster.toPageException(e);
+
+		}
+		
+		
+		try {
+			PBEKeySpec spec = new PBEKeySpec(passPhrase.toCharArray(), salt.getBytes(), (int)iterations, (int)keySize);
+			return Base64Coder.encode(key.generateSecret(spec).getEncoded());
+	    }
+	    catch (InvalidKeySpecException ikse) {
+	      throw Caster.toPageException(ikse);
+	    }
+	  }
+	
+	public static void main(String[] args) throws Exception {
+		/*for (Object obj : java.security.Security.getAlgorithms("Cipher")) {
+			  System.out.println(obj);
+			}
+		*/
+		
+		SecretKeyFactory pbeSkf = SecretKeyFactory.getInstance("PBKDF2WithSHA256", "JsafeJCE");
+
+		print.e(call(null, 					 "PBKDF2WithHmacSHA1" , "SSSS" , "salt"));
+		
+		
+		// 
+	}
+	
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if ( args.length == 5) return call( pc, Caster.toString(args[0]),Caster.toString(args[1]), Caster.toString(args[2]),Caster.toDoubleValue(args[3]),Caster.toDoubleValue(args[4]));
+		if ( args.length == 4) return call( pc, Caster.toString(args[0]),Caster.toString(args[1]), Caster.toString(args[2]),Caster.toDoubleValue(args[3]));
+		if ( args.length == 3) return call( pc, Caster.toString(args[0]),Caster.toString(args[1]), Caster.toString(args[2]));
+	    
+		throw new FunctionException(pc, "GeneratePBKDFKey", 3, 5, args.length);
+	}
+}
---
+++ test/tickets/LDEV0256.cfc
@@ -0,0 +1,69 @@
+component extends="org.lucee.cfml.test.LuceeTestCase" {
+
+	function beforeAll() {
+		variables.salt = 'A41n9t0Q';
+		variables.passphrase = 'passphrase';
+		variables.iterations = 100000;
+		variables.keysize = 2048;
+	}
+
+	function run( testResults , testBox ) {
+
+		describe( 'LDEV-256' , function() {
+
+			describe( 'GeneratePBKDFKey returns expected value' , function() {
+				it( 'for PBKDF2WithHmacSHA1' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithHmacSHA1' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'fRtApvaLO05oqIFHi86g1oyk9As2CfykHPW62wI3T4zl8aLbuTXHL2Z9cNQt/afv1uSsOcqRF/zjlP5Nfo4I4RcFOi+xK8Odixj5S5V7F6p3wN997NZNi9awWT/pBaPEnM2s4MaZclZMghKPGH2LMSF5ysf0VOIlDk30z+mJz3k62wlquISWgiPt7k73lZOsZCpLxQ0SJqFaIHiUm5xskpGt8nR1d8eHDOu7RhwjdjV40aPSowWxRWkIYtiv7gxCV2z1LZIz8B3JCVwfhElC0jS2z5jTpsGNWMGYifEOCCIRUNPPNxFkqehcBJc/ojQhCIKBlkDtFWp9OpslWdfnQw=='
+					);
+				}); 
+
+				/*it( 'for PBKDF2WithSHA1' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithSHA1' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'fRtApvaLO05oqIFHi86g1oyk9As2CfykHPW62wI3T4zl8aLbuTXHL2Z9cNQt/afv1uSsOcqRF/zjlP5Nfo4I4RcFOi+xK8Odixj5S5V7F6p3wN997NZNi9awWT/pBaPEnM2s4MaZclZMghKPGH2LMSF5ysf0VOIlDk30z+mJz3k62wlquISWgiPt7k73lZOsZCpLxQ0SJqFaIHiUm5xskpGt8nR1d8eHDOu7RhwjdjV40aPSowWxRWkIYtiv7gxCV2z1LZIz8B3JCVwfhElC0jS2z5jTpsGNWMGYifEOCCIRUNPPNxFkqehcBJc/ojQhCIKBlkDtFWp9OpslWdfnQw=='
+					);
+				}); 
+
+				it( 'for PBKDF2WithSHA224' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithSHA224' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'7+S9VMy0TyX4/2yTYOl7rlln1+GM9nxv3sTxpnE/g7VEOOMIkyuQu3GSN+UAocj1wON7iwPZq4GudRJg9VFswySCJ3SRp1DuTE4sITeoguFvw1DCnzFFAiWB7WRK/lwhbKOS8bdQ+/oZN6KIvZge54j5g4jlUFCJnRkUP8UHb9MdMKf/k4Q00IIS3tIe4SbBCNH+tNevFj9Y6ENMsUSoRS6vrzjh3FTkL2ldBRjixY59qkDxpeYWTIL2jWDsS5GvnRslP1zOFDbr+uCYbQJrWNR1hWFk8VjImsct/hrggw3wZ7qJXIgupIVGdra1tTIOtqsfani5t66r/oUPgkm0KQ=='
+					);
+				}); 
+
+				it( 'for PBKDF2WithSHA256' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithSHA256' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'pn/As0rErw6lrprnur+b1TaTszMXkoetNCceI01x+dRZDOBQHN0BYIUIlxmAUcrVGT8YvJE/AvlRuNVVeaXcm2D9ldOdXZ6dEIGdaY4ovQ88aiI3mZSHK6cPvxGRQ5KZA/YymWvBa/NZvHb0JWaZHZsygbXMCAIi3ZYECBonoRd4+s+R0isqjCjeTWZoFqIum78XkTz3zMSLcNMhwr9rVzUrj+WAxLYsx9PU5IC1X5gJ5cRU7e6B3zo6MMJ1qs777hqzCKAyrG6nw93YTinpRWfkPmWJ2aGmFWBj+y5N5d4kI8nv0a+U4G2m6CVtaI1VIaObliSD+CG6yuP4ylOKKQ=='
+					);
+				}); 
+
+				it( 'for PBKDF2WithSHA384' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithSHA384' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'cCCChfIzBIdh8h6FSv/ULHXCgClCVwY1ScyZvRl4vFKaYQCNp2JIN9nMQbsKy/iO6QYbuVSgiouz8GVW3AcP+Tmp7OFOZerIf2PP9zGCgd2b3/GkdoUegcgxGREkkMHpuDSRwGwBrusb8bd1Uvt9+cMx8WMLGML8Q7LzDqZyj2Bt3dZHydncAEelWxMXfs+SrOyTdpiVXqHNOWQO2lYROs3RMUjYJmP6IrD4VLsLdOhHadpOqTWExa2uGOiZjlP5Rem3a5mM9tTH7lnxbh7XYRyeDyCQmpk0NxETVrOznclvARYlXTF3WBt/Qg2tw1HfjpoqOL8b5W1yilK8maQdhg=='
+					);
+				}); 
+
+				it( 'for PBKDF2WithSHA512' , function() {
+					expect(
+						GeneratePBKDFKey( 'PBKDF2WithSHA512' , passphrase , salt, iterations , keysize )
+					).toBe(
+						'ufg8j2Yzw7rKp/iyXYzFoNe7/BNdwa8OYvlX3oQw0HfTlj9Wm7FIxUnabHXqRpjxRIwmS42Aecn1wrWgdwZXzRLIQB0NtiUNPaTKsBu4jo2pMMxC+MVJiJAW3CEjJyQSi6ZLSSzLh0KPlFtcyrdfu/ZLcUmLVVmBLrIDuVKJwCiXOAuhOaz7NCasBRZGiQTdQreNFDUD00wPt/t2NkDwpUDfVRn7qZ10MS8gOYeo3D4kaE1DxAJpYejxwDMrvxoZdqImTSiE2nzQ+zDDgYMywkhyNYhHjyLYbI1D6t7Rcgr3vc6f7orwgho+M1Ko52caVoTN3c64ioNyI3qSqc5TjQ=='
+					);
+				}); */
+
+			});
+
+		});
+
+	}
+	
+} 
