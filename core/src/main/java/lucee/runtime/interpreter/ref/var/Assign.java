76cc01485246343f93e48e68ff3299760a2f7432
Michael Offner <michael@lucee.org>
2016-08-29 21:42:55 +0200

    improve limited mode of CFMLExpressionInterpreter

--- core/src/main/java/lucee/runtime/interpreter/ref/var/Assign.java
+++ core/src/main/java/lucee/runtime/interpreter/ref/var/Assign.java
@@ -30,22 +30,26 @@ public final class Assign extends RefSupport implements Ref {
 	
 	private Ref value;
 	private Set coll;
+	private final boolean limited;
 
 
-    public Assign(Ref coll, Ref value) throws ExpressionException {
+    public Assign(Ref coll, Ref value, boolean limited) throws ExpressionException {
         if(!(coll instanceof Set))
         	throw new InterpreterException("invalid assignment left-hand side ("+coll.getTypeName()+")");
         this.coll=(Set) coll;
         this.value=value;
+        this.limited=limited;
     }
 
-    public Assign(Set coll, Ref value) {
+    public Assign(Set coll, Ref value, boolean limited) {
         this.coll=coll;
         this.value=value;
+        this.limited=limited;
     }
 	
     @Override
     public Object getValue(PageContext pc) throws PageException {
+    	if(limited) throw new InterpreterException("invalid syntax, variables are not supported in a json string.");
         return coll.setValue(pc,value.getValue(pc));
 	}
 
