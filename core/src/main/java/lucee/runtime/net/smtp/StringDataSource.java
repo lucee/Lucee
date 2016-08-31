6c71c97793a6ba65eb636fd069de4d9179e569b5
Igal Sapir <dev@21solutions.net>
2016-08-30 22:47:26 -0700

    Solved issue with unwanted CRLF in email body
    Solved LDEV-545.  Also fixed issue with wraptext attribute of cfmail.

--- core/src/main/java/lucee/runtime/net/smtp/StringDataSource.java
+++ core/src/main/java/lucee/runtime/net/smtp/StringDataSource.java
@@ -4,17 +4,17 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  **/
 package lucee.runtime.net.smtp;
 
@@ -22,12 +22,14 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Scanner;
 
 import javax.activation.DataSource;
 
 import lucee.commons.io.CharsetUtil;
 import lucee.commons.lang.CharSet;
 
+import lucee.commons.lang.StringUtil;
 import org.apache.commons.lang.WordUtils;
 
 public final class StringDataSource implements DataSource {
@@ -36,8 +38,12 @@ public final class StringDataSource implements DataSource {
 	private String ct;
 	private CharSet charset;
 
+	public final static char CR = (char) 13;
+	public final static char LF = (char) 10;
+
 	public StringDataSource(String text, String ct, CharSet charset, int maxLineLength) {
-		this.text=WordUtils.wrap(text, maxLineLength);
+
+		this.text = wrapText(text, maxLineLength);
 		this.ct=ct;
 		this.charset=charset;
 	}
@@ -60,6 +66,28 @@ public final class StringDataSource implements DataSource {
 	@Override
 	public OutputStream getOutputStream() throws IOException {
 		throw new IOException("no access to write");
+	}
+
+	public static String wrapText(String text, int maxLineLength) {
+
+		if (StringUtil.isEmpty(text))
+			return "";
+
+		StringBuilder sb = new StringBuilder(text.length());
+		Scanner scanner = new Scanner(text);
+		String line;
+
+		while (scanner.hasNextLine()){
+
+			line = scanner.nextLine();
+			if (line.length() > maxLineLength)
+				line = WordUtils.wrap(line, maxLineLength);
+			sb.append(line)
+					.append(CR)
+					.append(LF);
+		}
+
+		return sb.toString();
 	}
 
 }
