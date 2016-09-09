3e610acd3825e2644de2f8e813d5795bd124fbd0
Michael Offner <michael@lucee.org>
2016-09-09 16:18:51 +0200

    improve handling of source path in generated byte code

--- core/src/main/java/lucee/transformer/bytecode/Page.java
+++ core/src/main/java/lucee/transformer/bytecode/Page.java
@@ -501,8 +501,10 @@ public final class Page extends BodyBase implements Root {
     	
     	cw.visit(Opcodes.V1_6, Opcodes.ACC_PUBLIC+Opcodes.ACC_FINAL, className, null, parent, null);
     	if(optionalPS!=null) {
-    		cw.visitSource(optionalPS.getPhyscalFile().getAbsolutePath(),
-    			"rel:"+optionalPS.getRealpathWithVirtual()); // when adding more use ; as delimiter
+    		cw.visitSource(optionalPS.getRealpathWithVirtual(),null); // when adding more use ; as delimiter
+    		
+    		//cw.visitSource(optionalPS.getPhyscalFile().getAbsolutePath(),
+        	//		"rel:"+optionalPS.getRealpathWithVirtual()); // when adding more use ; as delimiter
     	}
     	else {
     		//cw.visitSource("","rel:");
