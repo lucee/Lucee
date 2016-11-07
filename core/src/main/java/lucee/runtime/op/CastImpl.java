61479f3eac50118d9f67dba1015ed48245b2c73d
Michael Offner <michael@lucee.org>
2016-11-07 09:14:33 +0100

    add "toCredentials" method to Cast

--- core/src/main/java/lucee/runtime/op/CastImpl.java
+++ core/src/main/java/lucee/runtime/op/CastImpl.java
@@ -34,6 +34,8 @@ import lucee.commons.color.ColorCaster;
 import lucee.commons.io.CharsetUtil;
 import lucee.commons.io.res.Resource;
 import lucee.commons.io.res.util.ResourceUtil;
+import lucee.commons.security.Credentials;
+import lucee.commons.security.CredentialsImpl;
 import lucee.runtime.Component;
 import lucee.runtime.PageContext;
 import lucee.runtime.dump.DumpData;
@@ -43,6 +45,7 @@ import lucee.runtime.exp.CasterException;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.exp.PageRuntimeException;
 import lucee.runtime.op.date.DateCaster;
+import lucee.runtime.security.CredentialImpl;
 import lucee.runtime.type.Array;
 import lucee.runtime.type.Collection;
 import lucee.runtime.type.KeyImpl;
@@ -1046,5 +1049,10 @@ public final class CastImpl implements Cast {
 	@Override
 	public DumpData toDumpTable(Struct sct,String title,PageContext pageContext, int maxlevel, DumpProperties dp) {
 		return StructUtil.toDumpTable(sct, title, pageContext, maxlevel, dp);
+	}
+	
+	// FUTURE add to interface
+	public Credentials toCredentials(String username, String password) {
+		return CredentialsImpl.toCredentials(username, password);
 	}
 }
