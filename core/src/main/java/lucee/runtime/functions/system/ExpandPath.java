70b9ac2d5b22688a39dc76513423dbbe55359c07
Michael Offner <michael@lucee.org>
2017-08-18 18:40:07 +0200

    fixes https://luceeserver.atlassian.net/browse/LDEV-1462

--- core/src/main/java/lucee/runtime/functions/system/ExpandPath.java
+++ core/src/main/java/lucee/runtime/functions/system/ExpandPath.java
@@ -48,7 +48,7 @@ public final class ExpandPath implements Function {
 		relPath=prettifyPath(pc,relPath);
 		
         String contextPath = pc.getHttpServletRequest().getContextPath();
-        if ( !StringUtil.isEmpty( contextPath ) && relPath.startsWith( contextPath ) ) {
+        if ( !StringUtil.isEmpty( contextPath ) && relPath.startsWith( contextPath+"/" ) ) {
             boolean sws=StringUtil.startsWith(relPath, '/');
         	relPath = relPath.substring( contextPath.length() );
             if(sws && !StringUtil.startsWith(relPath, '/'))
