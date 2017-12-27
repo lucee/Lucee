8b9ccbdba97036c9ba419dc9ae21f1c87b9cc5e8
CF Mitrah <j_saravanamuthu@yahoo.com>
2017-12-27 12:16:21 +0530

    LDEV-339 : arrayContainsNoCase looks at item substring
    I've added test cases for this ticket & fixed the issue by changing the ArrayContainsNoCase.java a bit.

--- core/src/main/java/lucee/runtime/functions/arrays/ArrayContainsNoCase.java
+++ core/src/main/java/lucee/runtime/functions/arrays/ArrayContainsNoCase.java
@@ -22,9 +22,11 @@
 package lucee.runtime.functions.arrays;
 
 import lucee.runtime.PageContext;
+import lucee.runtime.exp.FunctionException;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.ext.function.BIF;
 import lucee.runtime.op.Caster;
+import lucee.runtime.op.Decision;
 import lucee.runtime.type.Array;
 import lucee.runtime.type.util.ArrayUtil;
 
@@ -32,16 +34,31 @@ public final class ArrayContainsNoCase extends BIF {
 
 	private static final long serialVersionUID = 4394078979692450076L;
 
-	public static double call(PageContext pc , Array array, Object value) throws PageException {
-		String str=Caster.toString(value,null);
-		if(str!=null) 
-			return ArrayUtil.arrayContainsIgnoreEmpty(array,str,true)+1;
-		return ArrayFind.call(pc, array, value);
+	public static double call(PageContext pc , Array array, Object value, boolean substringMatch) throws PageException {
+		if (substringMatch) {
+			if (!Decision.isSimpleValue(value))
+                throw new FunctionException( pc, "ArrayContainsNoCase", 3, "substringMatch", "substringMatch can not be true when the value that is searched for is a complex object" );
+            return callLegacy( pc, array, value );
+		}
+		return ArrayFindNoCase.call( pc, array, value );
 	}
+
+	public static double call(PageContext pc , Array array, Object value) throws PageException {
+        return call( pc, array, value, false );
+    }
 	
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if ( args.length > 2)
+    		return call( pc, Caster.toArray(args[0]),args[1], Caster.toBoolean( args[2] ) );
 		return call(pc,Caster.toArray(args[0]),args[1]);
 	}
 	
+	/* legacy implementation */
+    static double callLegacy(PageContext pc, Array array, Object value) throws PageException {
+        String str=Caster.toString(value,null);
+        if(str!=null)
+            return ArrayUtil.arrayContainsIgnoreEmpty(array,str,true)+1;
+        return ArrayFindNoCase.call(pc, array, value);
+    }
 }
