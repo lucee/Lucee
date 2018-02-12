74c0ff19437b64700cb3777513ac4653f3086a7f
Michael Offner <michael@lucee.org>
2018-02-12 14:51:29 +0100

    fixes https://luceeserver.atlassian.net/browse/LDEV-1549

--- core/src/main/java/lucee/commons/io/IOUtil.java
+++ core/src/main/java/lucee/commons/io/IOUtil.java
@@ -1044,12 +1044,13 @@ public static String toString(Resource file, String charset) throws IOException 
         	if(result.indexOf("tika")!=-1) {
         		String tmp = ResourceUtil.EXT_MT.get(ResourceUtil.getExtension(res, "").toLowerCase());
         		if(!StringUtil.isEmpty(tmp)) return tmp;
+        		if(!StringUtil.isEmpty(defaultValue)) return defaultValue;
         	}
         	return result;
         } 
         catch(Exception e) {
         	String tmp = ResourceUtil.EXT_MT.get(ResourceUtil.getExtension(res, "").toLowerCase());
-    		if(!StringUtil.isEmpty(tmp)) return tmp;
+    		if(tmp.indexOf("tika")==-1 && !StringUtil.isEmpty(tmp)) return tmp;
         	return defaultValue;
         }
 		finally {
