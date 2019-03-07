6f8421f4010adc20750922c7d6beff4331054d7c
Igal Sapir <dev@21solutions.net>
2019-03-07 12:24:42 -0800

    removed override from noop impl of abstract methods that were added in Servlet 4.0
    this causes some version mismatches between dev environment and build environment

--- core/src/main/java/lucee/runtime/net/http/ServletContextDummy.java
+++ core/src/main/java/lucee/runtime/net/http/ServletContextDummy.java
@@ -318,37 +318,37 @@ public class ServletContextDummy implements ServletContext {
 
 
     
-    @Override
+    
     public ServletRegistration.Dynamic addJspFile(String s, String s1) {
         return null;
     }
 
-    @Override
+
     public int getSessionTimeout() {
         return 0;
     }
 
-    @Override
+
     public void setSessionTimeout(int i) {
 
     }
 
-    @Override
+
     public String getRequestCharacterEncoding() {
         return null;
     }
 
-    @Override
+
     public void setRequestCharacterEncoding(String s) {
 
     }
 
-    @Override
+
     public String getResponseCharacterEncoding() {
         return null;
     }
 
-    @Override
+
     public void setResponseCharacterEncoding(String s) {
 
     }
