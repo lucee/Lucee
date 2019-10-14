7db438a652ff137f949e228d0cc2659f96ec555c
Michael Offner <michael@lucee.org>
2019-10-14 10:54:55 +0200

    adding argument includeEmptyFields to ListAppen and ListPrepend LDEV-2495

--- core/src/main/java/lucee/runtime/functions/list/ListPrepend.java
+++ core/src/main/java/lucee/runtime/functions/list/ListPrepend.java
@@ -21,35 +21,43 @@
  */
 package lucee.runtime.functions.list;
 
-import lucee.commons.lang.StringUtil;
 import lucee.runtime.PageContext;
 import lucee.runtime.exp.FunctionException;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.ext.function.BIF;
 import lucee.runtime.op.Caster;
+import lucee.runtime.type.util.ListUtil;
 
 public final class ListPrepend extends BIF {
 
 	private static final long serialVersionUID = -4252541560957800011L;
 
 	public static String call(PageContext pc, String list, String value) {
-		if (list.length() == 0) return value;
-		return new StringBuilder(value).append(',').append(list).toString();
+		return call(pc, list, value, ",", true);
 	}
 
 	public static String call(PageContext pc, String list, String value, String delimiter) {
+		return call(pc, list, value, delimiter, true);
+	}
+
+	public static String call(PageContext pc, String list, String value, String delimiter, boolean includeEmptyFields) {
 		if (list.length() == 0) return value;
-		if (StringUtil.isEmpty(delimiter)) {
-			return call(pc, list, value);
+		if (delimiter.length() == 0) return list;
+
+		char del = delimiter.charAt(0);
+		if (!includeEmptyFields) {
+			list = ListUtil.listRemoveEmpty(list, del);
+			value = ListUtil.listRemoveEmpty(value, del);
 		}
-		return new StringBuilder(value).append(delimiter.charAt(0)).append(list).toString();
+		return new StringBuilder(value).append(del).append(list).toString();
 	}
 
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
-		if (args.length == 2) return call(pc, Caster.toString(args[0]), Caster.toString(args[1]));
-		if (args.length == 3) return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), Caster.toString(args[2]));
+		if (args.length == 2) return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), ",", true);
+		if (args.length == 3) return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), Caster.toString(args[2]), true);
+		if (args.length == 4) return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), Caster.toString(args[2]), Caster.toBooleanValue(args[3]));
 
-		throw new FunctionException(pc, "ListPrepend", 1, 3, args.length);
+		throw new FunctionException(pc, "ListPrepend", 1, 4, args.length);
 	}
 }
--- core/src/main/java/lucee/runtime/util/FileList.java
+++ core/src/main/java/lucee/runtime/util/FileList.java
@@ -13,21 +13,23 @@ public class FileList {
 
 	public static void list(File file) throws IOException {
 		if (file.isFile()) {
-			if (file.toString().endsWith(".cfm") || file.toString().endsWith(".cfc")) {
+			// (file.toString().endsWith(".cfm") || file.toString().endsWith(".cfc"))
+			if ((file.toString().endsWith(".cfm") || file.toString().endsWith(".cfc") || file.toString().endsWith(".js") || file.toString().endsWith(".css"))
+					&& file.toString().indexOf("/old_") == -1) {
 				FileInputStream fis = new FileInputStream(file);
 				try {
 					String str = IOUtil.toString(fis, "UTF-8");
 					//
 					// if (str.indexOf("\"upload\"") != -1) print.e(file);
 					// else if (str.indexOf("'upload'") != -1) print.e(file);
-					if (str.indexOf("\"upload\"") != -1) print.e((++count) + " " + file);
+					// if (str.indexOf("filenameArray[filenameArray.length-1].toLowerCase() != 'jpg'") != -1)
+					// print.e((++count) + " " + file);
+					if (str.indexOf("newReleaseChangedArtwork") != -1) print.e((++count) + " " + file);
 				}
 				finally {
 					IOUtil.closeEL(fis);
 				}
-
 			}
-
 		}
 		else if (file.isDirectory()) {
 			File[] children = file.listFiles();
