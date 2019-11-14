2e987b6487528693ed01f96bc139f844dd84e1a6
CF Mitrah <j_saravanamuthu@yahoo.com>
2019-11-14 16:55:13 +0530

    Added a additional condition for LDEV-1503

--- core/src/main/java/lucee/runtime/interpreter/CFMLExpressionInterpreter.java
+++ core/src/main/java/lucee/runtime/interpreter/CFMLExpressionInterpreter.java
@@ -1000,13 +1000,16 @@ public class CFMLExpressionInterpreter {
 		if (!cfml.isCurrent(start)) return null;
 
 		String[] str = cfml.toString().split(",");
-		if(cfml.getCurrent() == '{') {
-			for(int i=0; i<str.length; i++) {
+		if(cfml.getCurrent() == '{' && cfml.getNext() != '}' && str.length >1) {
+			outer:for(int i=0; i<str.length; i++) {
 				String strr = str[i].toString();
 				if(str[i].charAt(0) == '{') strr = new StringBuilder(strr).deleteCharAt(0).toString();
-				String[] strsplit = strr.split("[:=]");
-				System.out.println("dig:" + Character.isDigit(strsplit[1].toString().charAt(0)));
-				if(strsplit[0].charAt(0) != '"' || (strsplit[1].charAt(0) != '"' && Character.isDigit(strsplit[1].toString().charAt(0)) != true && strsplit[1].charAt(0) != '[')) {
+				String[] strsplit = strr.split("[:]");
+				if((strsplit[1].charAt(0) == '{' || strsplit[1].charAt(0) == '[') && strsplit[0].charAt(0) == '"') {
+					str = strsplit[1].toString().split(",");
+					continue outer;
+				}
+				else if(strsplit[0].charAt(0) != '"' || (strsplit[1].charAt(0) != '"' && !Character.isDigit(strsplit[1].charAt(0)) && strsplit[1].charAt(0) != '[')) {
 					throw new TemplateException("Invalid json value" +cfml);
 				}
 			}
