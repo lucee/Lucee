6cfcd71f69105d96dcee7a8bb49f01ce1263ddaf
Mircea Botez <mircea@lucee.org>
2020-05-31 21:22:57 +0300

    https://luceeserver.atlassian.net/browse/LDEV-2628 - fixed replaceMap

--- core/src/main/java/lucee/commons/lang/StringUtil.java
+++ core/src/main/java/lucee/commons/lang/StringUtil.java
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public 
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *
  **/
@@ -27,7 +27,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import lucee.print;
 import lucee.runtime.PageContext;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.op.Caster;
