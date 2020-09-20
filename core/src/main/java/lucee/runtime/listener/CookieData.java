5d03508fbfd5fb7d099646d43e3ebb01edfbd0c2
Zac Spitzer <zac.spitzer@gmail.com>
2020-09-20 16:26:45 +0200

    add SAMESITE_EMPTY LDEV-2993
    - changed the default for samesite not to be NONE
    - add samesite EMPTY (default)
    - always set samesite none when defined

--- core/src/main/java/lucee/runtime/listener/CookieData.java
+++ core/src/main/java/lucee/runtime/listener/CookieData.java
@@ -4,9 +4,10 @@ import lucee.runtime.type.dt.TimeSpan;
 
 public interface CookieData {
 
-	public static short SAMESITE_NONE = 0;
-	public static short SAMESITE_STRICT = 1;
-	public static short SAMESITE_LAX = 2;
+	public static short SAMESITE_EMPTY = 0;
+	public static short SAMESITE_NONE = 1;
+	public static short SAMESITE_STRICT = 2;
+	public static short SAMESITE_LAX = 3;
 
 	public abstract TimeSpan getTimeout();
 
