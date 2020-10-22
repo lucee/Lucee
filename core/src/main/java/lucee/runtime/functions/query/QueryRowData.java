34560be98ca1d1e73fb2047b815aec5d55b42ea6
Zac Spitzer <zac.spitzer@gmail.com>
2020-10-22 09:24:26 +0200

    allow returning an array from QueryRowData

--- core/src/main/java/lucee/runtime/functions/query/QueryRowData.java
+++ core/src/main/java/lucee/runtime/functions/query/QueryRowData.java
@@ -28,13 +28,22 @@ import lucee.runtime.type.Collection;
 import lucee.runtime.type.Query;
 import lucee.runtime.type.Struct;
 import lucee.runtime.type.StructImpl;
+import lucee.runtime.type.Array;
+import lucee.runtime.type.ArrayImpl;
 
 /**
  * implements BIF QueryRowData
  */
 public class QueryRowData extends BIF {
 
-	public static Struct call(PageContext pc, Query query, double rowNumber) throws PageException {
+	// is this needed?
+	private static final long serialVersionUID = -5234853923691806118L;	
+
+	public static Object call(PageContext pc, Query query, double rowNumber) throws PageException {
+		return call(pc, query, rowNumber, "struct");
+	}
+
+	public static Object call(PageContext pc, Query query, double rowNumber, String returnFormat) throws PageException {
 
 		int row = Caster.toInteger(rowNumber);
 
@@ -43,6 +52,16 @@ public class QueryRowData extends BIF {
 
 		Collection.Key[] colNames = query.getColumnNames();
 
+		if (returnFormat != null){
+			if ("array".equalsIgnoreCase(returnFormat.trim())) {
+				Array resultArray = new ArrayImpl();
+				for (int col = 0; col < colNames.length; col++){
+					resultArray.setE(col, query.getAt(colNames[col], row, NullSupportHelper.empty(pc)));
+				}
+				return resultArray;
+			} 
+			// defaults back to Struct, could check and throw?
+		}
 		Struct result = new StructImpl();
 
 		for (int col = 0; col < colNames.length; col++)
@@ -53,7 +72,7 @@ public class QueryRowData extends BIF {
 
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
-
-		return call(pc, Caster.toQuery(args[0]), Caster.toInteger(args[1]));
+		if (args.length == 2) return call(pc, Caster.toQuery(args[0]), Caster.toDouble(args[1]));
+		else return call(pc, Caster.toQuery(args[0]), Caster.toInteger(args[1]), Caster.toString(args[2]));
 	}
 }
