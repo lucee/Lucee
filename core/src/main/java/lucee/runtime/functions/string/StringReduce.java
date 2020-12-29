4795c3d5219deec52f2eca13cb8b5ec08c6e853a
mircea-botez <64434517+mircea-botez@users.noreply.github.com>
2020-12-29 17:12:41 +0200

    Update StringReduce.java

--- core/src/main/java/lucee/runtime/functions/string/StringReduce.java
+++ core/src/main/java/lucee/runtime/functions/string/StringReduce.java
@@ -14,18 +14,17 @@ public class StringReduce extends BIF {
 
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
-		if (args.length < 2) {
-			throw new FunctionException(pc, "StringSome", 2, 3, args.length);
+		if(args.length==2) {
+			return call(pc, Caster.toString(args[0]), Caster.toFunction(args[1]), null);
+		} else if(args.length==3) {
+			return call(pc, Caster.toString(args[0]), Caster.toFunction(args[1]), args[2]);
+		} else {
+			throw new FunctionException(pc, "StringReduce", 2, 3, args.length);
 		}
-		return call(pc, Caster.toString(args[0]), Caster.toFunction(args[1]), args[2]);
 	}
 
-	public static Object call(PageContext pc, String inputString, Object value, Object initialValue) throws PageException {
+	public static Object call(PageContext pc, String inputString, UDF value, Object initialValue) throws PageException {
 		StringListData stringList = new StringListData(inputString, "", false, false);
-		if (value instanceof UDF) {
-			return Reduce.call(pc, (Object) stringList, (UDF) value, initialValue);
-		}
-
-		throw new FunctionException(pc, "StringReduce", "2", "callback", "The callback argument is wrong", "");
+		return Reduce.call(pc, (Object) stringList, value, initialValue);
 	}
 }
