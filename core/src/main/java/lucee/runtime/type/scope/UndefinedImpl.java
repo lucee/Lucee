9f6d1ed1189398de0b6aaee449f0aa65bbcdefda
Saravanamuthu Aka CF Mitrah <j_saravanamuthu@yahoo.com>
2021-04-16 17:10:36 +0530

    Update UndefinedImpl.java

--- core/src/main/java/lucee/runtime/type/scope/UndefinedImpl.java
+++ core/src/main/java/lucee/runtime/type/scope/UndefinedImpl.java
@@ -236,7 +236,7 @@ public final class UndefinedImpl extends StructSupport implements Undefined {
 		}
 		String msg = ExceptionUtil.similarKeyMessage(this, key.getString(), "key", "keys", null, false);
 		String detail = ExceptionUtil.similarKeyMessage(this, key.getString(), "keys", null, false);
-		if (pc.getConfig().debug()) throw new ExpressionException(msg+ " " +detail);
+		if (pc.getConfig().debug()) throw new ExpressionException(msg, detail);
 
 		throw new ExpressionException("variable [" + key.getString() + "] doesn't exist");
 	}
