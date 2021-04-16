7f2ac11bea3a712deab3ed3b408047895d75c429
Michael Offner <michael@lucee.org>
2021-04-16 16:19:50 +0200

    LDEV-3030  - add a proper exception message when JDBC 3.0 is used that does not support this feature

--- core/src/main/java/lucee/runtime/query/caster/SQLXMLCast.java
+++ core/src/main/java/lucee/runtime/query/caster/SQLXMLCast.java
@@ -1,15 +1,32 @@
 package lucee.runtime.query.caster;
 
-import java.io.IOException;
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.TimeZone;
 
+import lucee.runtime.exp.DatabaseException;
+import lucee.runtime.exp.PageRuntimeException;
+import lucee.runtime.op.Caster;
+
 public class SQLXMLCast implements Cast {
 
 	@Override
-	public Object toCFType(TimeZone tz, ResultSet rst, int columnIndex) throws SQLException, IOException {
-		return rst.getSQLXML(columnIndex).getString();
+	public Object toCFType(TimeZone tz, ResultSet rst, int columnIndex) throws SQLException {
+
+		try {
+			return rst.getSQLXML(columnIndex).getString();
+		}
+		catch (SQLException se) {
+			throw se;
+		}
+		catch (Exception e) {
+			DatabaseMetaData md = rst.getStatement().getConnection().getMetaData();
+			if (md.getJDBCMajorVersion() < 4) throw new PageRuntimeException(
+					new DatabaseException("The data type [SQLXML] is not supported with this datasource.", "The datasource JDBC driver compatibility is up to the versions ["
+							+ md.getJDBCMajorVersion() + "." + md.getJDBCMinorVersion() + "], but this feature needs at least [4.0]", null, null));
+			throw new PageRuntimeException(Caster.toPageException(e));
+		}
 	}
 
 }
