0791369bb607ac1b167ee3154e5c06ce7ce6baa0
Tyler Clendenin <tylerc@gslsolutions.com>
2021-04-28 11:55:32 -0400

    LDEV-2855 Slight refactor for clarity

--- core/src/main/java/lucee/runtime/type/QueryImpl.java
+++ core/src/main/java/lucee/runtime/type/QueryImpl.java
@@ -128,7 +128,7 @@ public class QueryImpl implements Query, Objects, QueryResult {
 
 	private QueryColumnImpl[] columns;
 	private Collection.Key[] columnNames;
-	private ResultSetMetaData metaData;
+	private ResultSetMetaData metadata;
 	private SQL sql;
 	private Map<Integer, Integer> currRow = new ConcurrentHashMap<Integer, Integer>();
 	private int recordcount = 0;
@@ -414,7 +414,6 @@ public class QueryImpl implements Query, Objects, QueryResult {
 
 		try {
 			ResultSetMetaData meta = result.getMetaData();
-			if (qry != null) qry.setMetaData(meta);
 			columncount = meta.getColumnCount();
 
 			// set header arrays
@@ -567,6 +566,7 @@ public class QueryImpl implements Query, Objects, QueryResult {
 				qry.recordcount = recordcount;
 				qry.columnNames = columnNames;
 				qry.columns = columns;
+				qry.metadata = meta;
 			}
 			else {
 				qr.setColumnNames(columnNames);
@@ -2798,11 +2798,11 @@ public class QueryImpl implements Query, Objects, QueryResult {
 
 	@Override
 	public ResultSetMetaData getMetaData() {
-		return this.metaData;
+		return this.metadata;
 	}
 
-	private void setMetaData(ResultSetMetaData metaData) {
-		this.metaData = metaData;
+	private void setMetaData(ResultSetMetaData metadata) {
+		this.metadata = metadata;
 	}
 
 	@Override
@@ -2838,7 +2838,7 @@ public class QueryImpl implements Query, Objects, QueryResult {
 			this.name = other.name;
 			this.recordcount = other.recordcount;
 			this.sql = other.sql;
-			this.metaData = other.metaData;
+			this.metadata = other.metadata;
 			this.updateCount = other.updateCount;
 
 		}
@@ -3145,7 +3145,7 @@ public class QueryImpl implements Query, Objects, QueryResult {
 			}
 			newResult.currRow = new ConcurrentHashMap<Integer, Integer>();
 			newResult.sql = qry.getSql();
-			newResult.metaData = qry.getMetaData();
+			newResult.metadata = qry.getMetaData();
 			if (qry instanceof QueryImpl) newResult.templateLine = ((QueryImpl) qry).getTemplateLine();
 			else newResult.templateLine = new TemplateLine(qry.getTemplate(), 0);
 			newResult.recordcount = qry.getRecordcount();
