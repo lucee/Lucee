085a44b1c94a357678f756891bd5cc3005cfd1de
CF Mitrah <j_saravanamuthu@yahoo.com>
2021-06-25 21:09:43 +0530

    Added a new argument to define image column as image Object or base64 string

--- core/src/main/java/lucee/runtime/functions/system/ExtensionList.java
+++ core/src/main/java/lucee/runtime/functions/system/ExtensionList.java
@@ -19,23 +19,29 @@ public class ExtensionList extends BIF {
 	private static final long serialVersionUID = 3853910569001016577L;
 
 	public static Query call(PageContext pc) throws PageException {
+		return call(pc, false);
+	}
+	
+	public static Query call(PageContext pc, boolean imageObject) throws PageException {
 		ConfigPro config = (ConfigPro) pc.getConfig();
 
 		Query qry = RHExtension.toQuery(config, ((ConfigWebPro) pc.getConfig()).getServerRHExtensions(), null);
 		RHExtension.toQuery(config, ((ConfigWebPro) pc.getConfig()).getRHExtensions(), qry);
-		try {
-			for(int i=1; i <= qry.getRecordcount(); i++){
-				Object image = qry.getAt("image", i, null);
-				if( !StringUtil.isEmpty(image, true) ) {
-					byte[] imgByte = Caster.toBinary(image);
-					ImageUtil img = new ImageUtil();
-					Method m = img.getClass().getDeclaredMethod("toImage",new Class[] { PageContext.class, Object.class, boolean.class });
-					m.setAccessible(true);
-					qry.setAt("image", i, m.invoke(null, new Object[] { pc, imgByte, false }));
+		if(imageObject) {
+			try {
+				for(int i=1; i <= qry.getRecordcount(); i++){
+					Object image = qry.getAt("image", i, null);
+					if( !StringUtil.isEmpty(image, true) ) {
+						byte[] imgByte = Caster.toBinary(image);
+						ImageUtil img = new ImageUtil();
+						Method m = img.getClass().getDeclaredMethod("toImage",new Class[] { PageContext.class, Object.class, boolean.class });
+						m.setAccessible(true);
+						qry.setAt("image", i, m.invoke(null, new Object[] { pc, imgByte, false }));
+					}
 				}
 			}
-		}
-		catch (Exception e) {
+			catch (Exception e) {
+			}
 		}
 		return qry;
 	}
@@ -43,6 +49,7 @@ public class ExtensionList extends BIF {
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
 		if (args.length == 0) return call(pc);
-		else throw new FunctionException(pc, "ExtensionList", 0, 0, args.length);
+		if (args.length == 1) return call(pc, Caster.toBoolean(args[0]));
+		else throw new FunctionException(pc, "ExtensionList", 0, 1, args.length);
 	}
 }
