93017ea01cd3128efc229dadb2c438d722ce48f1
CF Mitrah <j_saravanamuthu@yahoo.com>
2021-07-06 19:23:58 +0530

    Recreate a DirectoryInfo file and added a key "mode" for linux

---
+++ core/src/main/java/lucee/runtime/functions/file/DirectoryInfo.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * Copyright (c) 2014, the Railo Company Ltd. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+package lucee.runtime.functions.file;
+
+import lucee.commons.io.res.Resource;
+import lucee.commons.io.res.util.ResourceUtil;
+import lucee.runtime.PageContext;
+import lucee.runtime.exp.PageException;
+import lucee.runtime.tag.Directory;
+import lucee.runtime.type.Struct;
+
+public class DirectoryInfo {
+    public static Struct call(PageContext pc, String path) throws PageException {
+        return Directory.getInfo(pc, ResourceUtil.toResourceExisting(pc, path), null);
+    }
+}
