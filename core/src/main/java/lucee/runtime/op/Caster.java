63de02e0ab52f9617b589a81d527284477cab164
Michael Offner <michael@lucee.org>
2021-09-10 14:26:45 +0200

    make sure the Caster has all necessary methods, so the compiler can choose a better/faster match

--- core/src/main/java/lucee/runtime/op/Caster.java
+++ core/src/main/java/lucee/runtime/op/Caster.java
@@ -391,7 +391,7 @@ public final class Caster {
 	private static final Object DEFAULT = new Object();
 	static {
 		for (int i = 0; i < MAX_SMALL_DOUBLE; i++)
-			smallDoubles[i] = new Double(i);
+			smallDoubles[i] = Double.valueOf(i);
 	}
 
 	public static Double toDouble(double d) {
@@ -399,7 +399,11 @@ public final class Caster {
 			int i;
 			if ((i = ((int) d)) == d) return smallDoubles[i];
 		}
-		return new Double(d);
+		return Double.valueOf(d);
+	}
+
+	public static Double toDouble(Number n) {
+		return n.doubleValue();
 	}
 
 	/**
@@ -409,7 +413,11 @@ public final class Caster {
 	 * @return casted Double Object
 	 */
 	public static Double toDouble(boolean b) {
-		return new Double(b ? 1 : 0);
+		return Double.valueOf(b ? 1 : 0);
+	}
+
+	public static Double toDouble(Boolean b) {
+		return Double.valueOf(b ? 1 : 0);
 	}
 
 	/**
@@ -693,6 +701,10 @@ public final class Caster {
 		return d;
 	}
 
+	public static double toDoubleValue(Number n) {
+		return n.doubleValue();
+	}
+
 	public static double toDoubleValue(float f) {
 		return f;
 	}
@@ -708,6 +720,10 @@ public final class Caster {
 	 * @return casted double value
 	 */
 	public static double toDoubleValue(boolean b) {
+		return b ? 1 : 0;
+	}
+
+	public static double toDoubleValue(Boolean b) {
 		return b ? 1 : 0;
 	}
 
@@ -805,6 +821,10 @@ public final class Caster {
 		return (int) d;
 	}
 
+	public static int toIntValue(Number n) {
+		return n.intValue();
+	}
+
 	/**
 	 * cast an int value to an int value (do nothing)
 	 * 
@@ -822,6 +842,10 @@ public final class Caster {
 	 * @return casted int value
 	 */
 	public static int toIntValue(boolean b) {
+		return b ? 1 : 0;
+	}
+
+	public static int toIntValue(Boolean b) {
 		return b ? 1 : 0;
 	}
 
@@ -952,6 +976,10 @@ public final class Caster {
 	 */
 	public static Boolean toBoolean(boolean b) {
 		return b ? Boolean.TRUE : Boolean.FALSE;
+	}
+
+	public static Boolean toBoolean(Boolean b) {
+		return b;
 	}
 
 	/**
@@ -1089,6 +1117,10 @@ public final class Caster {
 		return (char) (b ? 1 : 0);
 	}
 
+	public static char toCharValue(Boolean b) {
+		return (char) (b.booleanValue() ? 1 : 0);
+	}
+
 	/**
 	 * cast a double value to a char value (primitive value type)
 	 * 
@@ -1097,6 +1129,10 @@ public final class Caster {
 	 */
 	public static char toCharValue(double d) {
 		return (char) d;
+	}
+
+	public static char toCharValue(Number n) {
+		return (char) n.intValue();
 	}
 
 	/**
@@ -1178,7 +1214,11 @@ public final class Caster {
 	 * @return casted Character Object
 	 */
 	public static Character toCharacter(boolean b) {
-		return new Character(toCharValue(b));
+		return Character.valueOf(toCharValue(b));
+	}
+
+	public static Character toCharacter(Boolean b) {
+		return Character.valueOf(toCharValue(b));
 	}
 
 	/**
@@ -1199,6 +1239,10 @@ public final class Caster {
 	 */
 	public static Character toCharacter(double d) {
 		return new Character(toCharValue(d));
+	}
+
+	public static Character toCharacter(Number n) {
+		return new Character(toCharValue(n));
 	}
 
 	/**
@@ -1252,6 +1296,10 @@ public final class Caster {
 		return (byte) (b ? 1 : 0);
 	}
 
+	public static byte toByteValue(Boolean b) {
+		return (byte) (b.booleanValue() ? 1 : 0);
+	}
+
 	/**
 	 * cast a double value to a byte value (primitive value type)
 	 * 
@@ -1260,6 +1308,10 @@ public final class Caster {
 	 */
 	public static byte toByteValue(double d) {
 		return (byte) d;
+	}
+
+	public static byte toByteValue(Number n) {
+		return n.byteValue();
 	}
 
 	/**
@@ -1330,7 +1382,11 @@ public final class Caster {
 	 * @return casted Byte Object
 	 */
 	public static Byte toByte(boolean b) {
-		return new Byte(toByteValue(b));
+		return Byte.valueOf(toByteValue(b));
+	}
+
+	public static Byte toByte(Boolean b) {
+		return Byte.valueOf(toByteValue(b));
 	}
 
 	/**
@@ -1350,7 +1406,11 @@ public final class Caster {
 	 * @return casted Byte Object
 	 */
 	public static Byte toByte(double d) {
-		return new Byte(toByteValue(d));
+		return Byte.valueOf(toByteValue(d));
+	}
+
+	public static Byte toByte(Number n) {
+		return Byte.valueOf(toByteValue(n));
 	}
 
 	/**
@@ -1403,6 +1463,10 @@ public final class Caster {
 		return (b ? 1L : 0L);
 	}
 
+	public static long toLongValue(Boolean b) {
+		return (b ? 1L : 0L);
+	}
+
 	/**
 	 * cast a double value to a long value (primitive value type)
 	 * 
@@ -1411,6 +1475,10 @@ public final class Caster {
 	 */
 	public static long toLongValue(double d) {
 		return (long) d;
+	}
+
+	public static long toLongValue(Number n) {
+		return n.longValue();
 	}
 
 	/**
@@ -1530,6 +1598,10 @@ public final class Caster {
 		return Long.valueOf(toLongValue(b));
 	}
 
+	public static Long toLong(Boolean b) {
+		return Long.valueOf(toLongValue(b.booleanValue()));
+	}
+
 	/**
 	 * cast a char value to a Long Object(reference type)
 	 * 
@@ -1548,6 +1620,10 @@ public final class Caster {
 	 */
 	public static Long toLong(double d) {
 		return Long.valueOf(toLongValue(d));
+	}
+
+	public static Long toLong(Number n) {
+		return Long.valueOf(n.longValue());
 	}
 
 	/**
@@ -1609,7 +1685,11 @@ public final class Caster {
 	 * @return casted Float Object
 	 */
 	public static Float toFloat(boolean b) {
-		return new Float(toFloatValue(b));
+		return Float.valueOf(toFloatValue(b));
+	}
+
+	public static Float toFloat(Boolean b) {
+		return Float.valueOf(toFloatValue(b.booleanValue()));
 	}
 
 	/**
@@ -1619,7 +1699,7 @@ public final class Caster {
 	 * @return casted Float Object
 	 */
 	public static Float toFloat(char c) {
-		return new Float(toFloatValue(c));
+		return Float.valueOf(toFloatValue(c));
 	}
 
 	/**
@@ -1686,6 +1766,10 @@ public final class Caster {
 	 * @return casted long value
 	 */
 	public static float toFloatValue(boolean b) {
+		return (b ? 1F : 0F);
+	}
+
+	public static float toFloatValue(Boolean b) {
 		return (b ? 1F : 0F);
 	}
 
@@ -1770,6 +1854,10 @@ public final class Caster {
 		return (short) (b ? 1 : 0);
 	}
 
+	public static short toShortValue(Boolean b) {
+		return (short) (b ? 1 : 0);
+	}
+
 	/**
 	 * cast a double value to a short value (primitive value type)
 	 * 
@@ -1778,6 +1866,10 @@ public final class Caster {
 	 */
 	public static short toShortValue(double d) {
 		return (short) d;
+	}
+
+	public static short toShortValue(Number n) {
+		return n.shortValue();
 	}
 
 	/**
@@ -1852,6 +1944,10 @@ public final class Caster {
 		return Short.valueOf(toShortValue(b));
 	}
 
+	public static Short toShort(Boolean b) {
+		return Short.valueOf(toShortValue(b.booleanValue()));
+	}
+
 	/**
 	 * cast a char value to a Short Object(reference type)
 	 * 
@@ -1870,6 +1966,10 @@ public final class Caster {
 	 */
 	public static Short toShort(double d) {
 		return Short.valueOf(toShortValue(d));
+	}
+
+	public static Short toShort(Number n) {
+		return Short.valueOf(n.shortValue());
 	}
 
 	/**
@@ -2234,6 +2334,10 @@ public final class Caster {
 	 * @return casted String
 	 */
 	public static String toString(boolean b) {
+		return b ? "true" : "false";
+	}
+
+	public static String toString(Boolean b) {
 		return b ? "true" : "false";
 	}
 
@@ -2941,6 +3045,10 @@ public final class Caster {
 		return DateCaster.toDateSimple(b, tz);
 	}
 
+	public static DateTime toDate(Boolean b, TimeZone tz) {
+		return DateCaster.toDateSimple(b, tz);
+	}
+
 	/**
 	 * cast a char to a DateTime Object
 	 * 
@@ -2961,6 +3069,10 @@ public final class Caster {
 	 */
 	public static DateTime toDate(double d, TimeZone tz) {
 		return DateCaster.toDateSimple(d, tz);
+	}
+
+	public static DateTime toDate(Number n, TimeZone tz) {
+		return DateCaster.toDateSimple(n.doubleValue(), tz);
 	}
 
 	/**
@@ -4528,6 +4640,10 @@ public final class Caster {
 		return b ? Constants.INTEGER_1 : Constants.INTEGER_0;
 	}
 
+	public static Integer toInteger(Boolean b) {
+		return b ? Constants.INTEGER_1 : Constants.INTEGER_0;
+	}
+
 	/**
 	 * casts a char to an Integer
 	 * 
@@ -4546,6 +4662,10 @@ public final class Caster {
 	 */
 	public static Integer toInteger(double d) {
 		return Integer.valueOf((int) d);
+	}
+
+	public static Integer toInteger(Number n) {
+		return Integer.valueOf(n.intValue());
 	}
 
 	/**
