56043d0692873e106700630f10796472f66e2279
Michael Offner <michael@lucee.org>
2021-11-05 18:18:59 +0100

    LDEV-442 - change formatting and set new version

--- core/src/main/java/lucee/runtime/functions/arrays/ArrayRemoveDuplicates.java
+++ core/src/main/java/lucee/runtime/functions/arrays/ArrayRemoveDuplicates.java
@@ -22,31 +22,32 @@ import lucee.runtime.PageContext;
 import lucee.runtime.exp.FunctionException;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.ext.function.BIF;
-import lucee.runtime.functions.arrays.ArrayFind;
 import lucee.runtime.op.Caster;
 import lucee.runtime.type.Array;
 import lucee.runtime.type.ArrayImpl;
 
 public final class ArrayRemoveDuplicates extends BIF {
-    
-    public static Array call(PageContext pc, Array arr) throws PageException {
-            return call(pc, arr, false);
-    }
 
-    public static Array call(PageContext pc, Array arr, boolean ignoreCase) throws PageException {
-        Array a = new ArrayImpl();
-        int i;
-        for(i = 1; i <= arr.size(); i++) {
-            Object value = arr.getE(i);
-            if(ArrayFind.find(a, value, !ignoreCase) == 0) a.appendEL(value);
-        }
-        return a; 
-    }
+	private static final long serialVersionUID = 1292207681027528972L;
 
-    @Override
-    public Object invoke(PageContext pc, Object[] args) throws PageException {
-        if (args.length == 1) return call(pc, Caster.toArray(args[0]));
-        else if (args.length == 2) return call(pc, Caster.toArray(args[0]), Caster.toBoolean(args[1]));
-        else throw new FunctionException(pc, "ArrayRemoveDuplicates", 1, 2 , args.length);
-    }
+	public static Array call(PageContext pc, Array arr) throws PageException {
+		return call(pc, arr, false);
+	}
+
+	public static Array call(PageContext pc, Array arr, boolean ignoreCase) throws PageException {
+		Array a = new ArrayImpl();
+		int i;
+		for (i = 1; i <= arr.size(); i++) {
+			Object value = arr.getE(i);
+			if (ArrayFind.find(a, value, !ignoreCase) == 0) a.appendEL(value);
+		}
+		return a;
+	}
+
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if (args.length == 1) return call(pc, Caster.toArray(args[0]));
+		else if (args.length == 2) return call(pc, Caster.toArray(args[0]), Caster.toBoolean(args[1]));
+		else throw new FunctionException(pc, "ArrayRemoveDuplicates", 1, 2, args.length);
+	}
 }
