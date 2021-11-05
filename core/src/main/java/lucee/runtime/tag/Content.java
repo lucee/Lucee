f7b1edb10e5dab07c351869df640511855615cfd
Michael Offner <michael@lucee.org>
2021-11-05 17:51:17 +0100

    LDEV-3742 - adapt fix from branch 5.3

--- core/src/main/java/lucee/runtime/tag/Content.java
+++ core/src/main/java/lucee/runtime/tag/Content.java
@@ -163,6 +163,13 @@ public final class Content extends BodyTagImpl {
 		// check the file before doing anything else
 		Resource file = null;
 		if (content == null && !StringUtil.isEmpty(strFile)) file = ResourceUtil.toResourceExisting(pageContext, strFile);
+		if (content == null && !StringUtil.isEmpty(strFile)) {
+			file = ResourceUtil.toResourceExisting(pageContext, strFile);
+			// Do not overwrite type-attribute
+			if (StringUtil.isEmpty(type, true)) {
+				type = ResourceUtil.getMimeType(file, "text/html");
+			}
+		}
 
 		// get response object
 		HttpServletResponse rsp = pageContext.getHttpServletResponse();
