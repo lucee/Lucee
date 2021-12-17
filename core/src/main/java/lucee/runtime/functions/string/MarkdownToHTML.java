6868698e9fedb702dc17b6a86a5146fbe37b78da
michaeloffner <michaeloffner@users.noreply.github.com>
2021-12-17 16:16:39 +0100

    LDEV-3783 - add function markdownToHTML

---
+++ core/src/main/java/lucee/runtime/functions/string/MarkdownToHTML.java
@@ -0,0 +1,72 @@
+package lucee.runtime.functions.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import com.github.rjeschke.txtmark.Processor;
+
+import lucee.print;
+import lucee.commons.io.CharsetUtil;
+import lucee.commons.io.IOUtil;
+import lucee.commons.io.res.Resource;
+import lucee.commons.io.res.util.ResourceUtil;
+import lucee.commons.lang.StringUtil;
+import lucee.runtime.PageContext;
+import lucee.runtime.engine.ThreadLocalPageContext;
+import lucee.runtime.exp.FunctionException;
+import lucee.runtime.exp.PageException;
+import lucee.runtime.ext.function.BIF;
+import lucee.runtime.ext.function.Function;
+import lucee.runtime.op.Caster;
+
+public class MarkdownToHTML extends BIF implements Function {
+
+	private static final long serialVersionUID = 3775127934350736736L;
+
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		if (args.length < 1 || args.length > 2) {
+			throw new FunctionException(pc, "MarkdownToHTML", 1, 2, args.length);
+		}
+		return call(pc, Caster.toString(args[0]));
+	}
+
+	public static String call(PageContext pc, String markdown) throws PageException {
+		return call(pc, markdown, false, null);
+	}
+
+	public static String call(PageContext pc, String markdown, boolean safeMode) throws PageException {
+		return call(pc, markdown, safeMode, null);
+	}
+
+	public static String call(PageContext pc, String markdown, boolean safeMode, String encoding) throws PageException {
+		if (markdown.length() < 2000 && !StringUtil.isEmpty(markdown, true)) {
+			Resource res = ResourceUtil.toResourceExisting(pc, markdown.trim(), false, null);
+			if (res != null) {
+				Charset cs;
+				if (StringUtil.isEmpty(encoding, true)) cs = ThreadLocalPageContext.getConfig(pc).getWebCharset();
+				else cs = CharsetUtil.toCharset(encoding.trim());
+
+				BufferedReader br = null;
+				try {
+					br = new BufferedReader(new InputStreamReader(res.getInputStream(), cs));
+					return Processor.process(br, safeMode);
+				}
+				catch (IOException e) {
+					throw Caster.toPageException(e);
+				}
+				finally {
+					IOUtil.closeEL(br);
+				}
+
+			}
+		}
+		return Processor.process(markdown, safeMode);
+	}
+
+	public static void main(String[] args) {
+		print.e(Processor.process("This is ***TXTMARK***", false));
+	}
+}
---
+++ test/functions/MarkdownToHTML.cfc
@@ -0,0 +1,70 @@
+component extends="org.lucee.cfml.test.LuceeTestCase" {
+	
+    variables.markdownString=trim(" 
+    #### Headline with ID ####
+
+Another headline with ID
+------------------------
+
+* List with ID 
+
+Links: [Foo] (##headid)
+
+This is ***TXTMARK***
+This is ***TXTMARK***
+    
+``
+This is code!
+``
+
+1. First list item
+   - First nested list item
+     - Second nested list item
+");
+
+variables.htmlString=trim("<h2>Headline with ID</h2>
+<h2>Another headline with ID</h2>
+<ul>
+<li>List with ID</li>
+</ul>
+<p>Links: <a href="#headid">Foo</a></p>
+<p>This is <strong><em>TXTMARK</em></strong>
+This is <strong><em>TXTMARK</em></strong></p>
+<p><code>
+This is code!
+</code></p>
+<ol>
+<li>First list item</li>
+<li>First nested list item<ul>
+<li>Second nested list item</li>
+</ul>
+</li>
+</ol>");
+	
+	
+	function run( testResults, testBox ){
+		describe( title="Testcase for markdownToHTML()", body=function() {
+			it( title = "Checking with markdownToHTML with a string", body=function( currentSpec ) {
+				assertEquals(
+					variables.htmlString,
+					markdownToHTML(variables.markdownString)
+				);
+			});
+
+			it( title = "Checking with markdownToHTML with a file", body=function( currentSpec ) {
+				var curr=getDirectoryFromPath(getCurrentTemplatePath());
+				var file=curr&"markdownToHTML/test.md";
+				try {
+					fileWrite(file, markdownString);
+					assertEquals(
+						variables.htmlString,
+						markdownToHTML(file)
+					);
+				}
+				finally {
+					if(fileExists(file)) fileDelete(file);
+				}
+			});
+		});
+	}
+}
