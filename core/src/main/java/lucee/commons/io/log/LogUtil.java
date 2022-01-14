3ef07e17c127ca76d5277068ccafaeb1c60a554d
michaeloffner <michaeloffner@users.noreply.github.com>
2022-01-14 18:30:32 +0100

    LDEV-1136 - extend alreadyInLog check

--- core/src/main/java/lucee/commons/io/log/LogUtil.java
+++ core/src/main/java/lucee/commons/io/log/LogUtil.java
@@ -68,7 +68,9 @@ public final class LogUtil {
 			String str;
 			for (StackTraceElement ste: stes) {
 				str = ste.getClassName();
-				if (ste.getClassName().indexOf("org.apache.log4j.") == 0 || ste.getClassName().indexOf("org.apache.logging.log4j.") == 0) return true;
+				if (ste.getClassName().indexOf("org.apache.log4j.") == 0 || ste.getClassName().indexOf("org.apache.logging.log4j.") == 0
+						|| ste.getClassName().indexOf("lucee.commons.io.log.log4j") == 0)
+					return true;
 			}
 		}
 		return false;
