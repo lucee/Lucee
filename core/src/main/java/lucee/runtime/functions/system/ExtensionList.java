74affa9cdc07dd8d39de1ef8a2ba75d3b0a8c9bd
michaeloffner <michaeloffner@users.noreply.github.com>
2022-01-17 11:43:21 +0100

    LDEV-3261 - use a different way to load the image

--- core/src/main/java/lucee/runtime/functions/system/ExtensionList.java
+++ core/src/main/java/lucee/runtime/functions/system/ExtensionList.java
@@ -1,18 +1,18 @@
 package lucee.runtime.functions.system;
 
-import java.lang.reflect.Method;
-
 import lucee.commons.lang.StringUtil;
+import lucee.loader.engine.CFMLEngine;
+import lucee.loader.engine.CFMLEngineFactory;
 import lucee.runtime.PageContext;
 import lucee.runtime.config.ConfigPro;
 import lucee.runtime.config.ConfigWebPro;
 import lucee.runtime.exp.FunctionException;
 import lucee.runtime.exp.PageException;
 import lucee.runtime.ext.function.BIF;
-import lucee.runtime.image.ImageUtil;
 import lucee.runtime.extension.RHExtension;
 import lucee.runtime.op.Caster;
 import lucee.runtime.type.Query;
+import lucee.runtime.type.util.KeyConstants;
 
 public class ExtensionList extends BIF {
 
@@ -21,22 +21,30 @@ public class ExtensionList extends BIF {
 	public static Query call(PageContext pc) throws PageException {
 		return call(pc, false);
 	}
-	
+
 	public static Query call(PageContext pc, boolean imageObject) throws PageException {
 		ConfigPro config = (ConfigPro) pc.getConfig();
+		BIF bif = null;
+		boolean bifLoaded = false;
 
 		Query qry = RHExtension.toQuery(config, ((ConfigWebPro) pc.getConfig()).getServerRHExtensions(), null);
 		RHExtension.toQuery(config, ((ConfigWebPro) pc.getConfig()).getRHExtensions(), qry);
-		if(imageObject) {
+		if (imageObject) {
 			try {
-				for(int i=1; i <= qry.getRecordcount(); i++){
+				for (int i = 1; i <= qry.getRecordcount(); i++) {
 					Object image = qry.getAt("image", i, null);
-					if( !StringUtil.isEmpty(image, true) ) {
-						byte[] imgByte = Caster.toBinary(image);
-						ImageUtil img = new ImageUtil();
-						Method m = img.getClass().getDeclaredMethod("toImage",new Class[] { PageContext.class, Object.class, boolean.class });
-						m.setAccessible(true);
-						qry.setAt("image", i, m.invoke(null, new Object[] { pc, imgByte, false }));
+					if (!StringUtil.isEmpty(image, true)) {
+						// image stuff is in a OPTIONAL extension
+						if (!bifLoaded) {
+							bif = getBIF(pc);
+							bifLoaded = true;
+						}
+						if (bif != null) {
+							Object res = bif.invoke(pc, new Object[] { image });
+							if (res != null) {
+								qry.setAt(KeyConstants._image, i, res);
+							}
+						}
 					}
 				}
 			}
@@ -44,6 +52,16 @@ public class ExtensionList extends BIF {
 			}
 		}
 		return qry;
+	}
+
+	private static BIF getBIF(PageContext pc) {
+		CFMLEngine eng = CFMLEngineFactory.getInstance();
+		try {
+			return eng.getClassUtil().loadBIF(pc, "org.lucee.extension.image.functions.ImageRead");
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
