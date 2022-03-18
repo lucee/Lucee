823f1e88cc7091310465d8b92c3d4da4d0add331
michaeloffner <michaeloffner@users.noreply.github.com>
2022-03-18 15:32:11 +0100

    LDEV-3910 - fix invalid boolean operation

--- core/src/main/java/lucee/runtime/tag/Content.java
+++ core/src/main/java/lucee/runtime/tag/Content.java
@@ -182,7 +182,7 @@ public final class Content extends BodyTagImpl {
 			ReqRspUtil.setContentType(rsp, type);
 
 			// TODO more dynamic implementation, configuration in admin?
-			if (!HTTPUtil.isTextMimeType(type) == Boolean.TRUE) {
+			if (HTTPUtil.isTextMimeType(type) == Boolean.FALSE) {
 				((PageContextImpl) pageContext).getRootOut().setAllowCompression(false);
 			}
 		}
