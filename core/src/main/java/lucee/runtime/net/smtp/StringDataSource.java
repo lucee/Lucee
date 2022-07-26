b5360dcb581c0d214753c01232627925b1a69b18
Dan G. Switzer, II <dswitzer@pengoworks.com>
2022-07-26 11:35:32 -0400

    Updated HTML email parts to use quoted-printable transfer encoding
    * Updated HTML email parts to use quoted-printable content transfer encoding
    * Updated StringDataSource to allow non-wrapping of text source
    * Added unit test coverage for changes (you need to rename _Mail.cfc to Mail.cfc for the tests to run)

--- core/src/main/java/lucee/runtime/net/smtp/StringDataSource.java
+++ core/src/main/java/lucee/runtime/net/smtp/StringDataSource.java
@@ -41,8 +41,18 @@ public final class StringDataSource implements DataSource {
 	public final static char CR = (char) 13;
 	public final static char LF = (char) 10;
 
-	public StringDataSource(String text, String ct, CharSet charset, int maxLineLength) {
+	/*
+	 * Some types of transfer encoding such as "quoted-printable" and "base64"
+	 * do not require wrapping of lines, because it's handled automatically
+	 * in the encoding.
+	 */
+	public StringDataSource(String text, String ct, CharSet charset) {
+		this.text = text;
+		this.ct = ct;
+		this.charset = charset;
+	}
 
+	public StringDataSource(String text, String ct, CharSet charset, int maxLineLength) {
 		this.text = wrapText(text, maxLineLength);
 		this.ct = ct;
 		this.charset = charset;
