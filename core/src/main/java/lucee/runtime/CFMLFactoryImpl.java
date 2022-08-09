341ce85d725c8ad24a65475a1be3a75ddca9732d
michaeloffner <michaeloffner@users.noreply.github.com>
2022-08-09 16:32:55 +0200

    change max norm prio to 0

--- core/src/main/java/lucee/runtime/CFMLFactoryImpl.java
+++ core/src/main/java/lucee/runtime/CFMLFactoryImpl.java
@@ -81,7 +81,7 @@ public final class CFMLFactoryImpl extends CFMLFactory {
 
 	private static final long MAX_AGE = 5 * 60000; // 5 minutes
 	private static final int MAX_SIZE = 10000;
-	private static final int MAX_NORMAL_PRIORITY = 5;
+	private static final int MAX_NORMAL_PRIORITY = 0;
 	private static final int MAX_NO_SLEEP = 0;
 	private static final int SLEEP_TIME = 100;
 	private static JspEngineInfo info = new JspEngineInfoImpl("1.0");
