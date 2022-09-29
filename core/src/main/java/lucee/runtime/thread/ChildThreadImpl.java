f2fc56faa326517b74e8a11a6987a48cf051ef88
CF Mitrah <j_saravanamuthu@yahoo.com>
2022-09-29 15:15:59 +0530

    Added a fix to thread debug logging to entry the total time for LDEV-4216

--- core/src/main/java/lucee/runtime/thread/ChildThreadImpl.java
+++ core/src/main/java/lucee/runtime/thread/ChildThreadImpl.java
@@ -36,8 +36,11 @@ import lucee.runtime.PageContext;
 import lucee.runtime.PageContextImpl;
 import lucee.runtime.PageSourceImpl;
 import lucee.runtime.config.Config;
+import lucee.runtime.config.ConfigImpl;
+import lucee.runtime.config.ConfigPro;
 import lucee.runtime.config.ConfigWeb;
 import lucee.runtime.config.ConfigWebPro;
+import lucee.runtime.debug.DebugEntryTemplate;
 import lucee.runtime.engine.ThreadLocalPageContext;
 import lucee.runtime.exp.Abort;
 import lucee.runtime.exp.PageException;
@@ -151,6 +154,8 @@ public class ChildThreadImpl extends ChildThread implements Serializable {
 		PageContext oldPc = ThreadLocalPageContext.get();
 		Page p = page;
 		PageContextImpl pc = null;
+		DebugEntryTemplate debugEntry = null;
+		long time = System.nanoTime();
 		try {
 			// daemon
 			if (this.pc != null) {
@@ -175,6 +180,10 @@ public class ChildThreadImpl extends ChildThread implements Serializable {
 				}
 				pc.addPageSource(p.getPageSource(), true);
 			}
+
+			ConfigImpl ci = (ConfigImpl) pc.getConfig();
+			if (!pc.isGatewayContext() && ci.debug() && ci.hasDebugOptions(ConfigPro.DEBUG_TEMPLATE)) 
+				debugEntry = pc.getDebugger().getEntry(pc, page.getPageSource());
 
 			threadScope = pc.getCFThreadScope();
 			pc.setCurrentThreadScope(new ThreadsImpl(this));
@@ -236,6 +245,8 @@ public class ChildThreadImpl extends ChildThread implements Serializable {
 			}
 		}
 		finally {
+			if (debugEntry != null) debugEntry.updateExeTime(System.nanoTime() - time);
+			pc.setEndTimeNS(System.nanoTime());
 			endTime = System.currentTimeMillis();
 			pc.getConfig().getFactory().releaseLuceePageContext(pc, true);
 			pc = null;
