28af8b097b3f9b290a63104591aa8b025e8ebef2
Igal Sapir <dev@21solutions.net>
2022-12-28 23:33:05 -0800

    LDEV-2805 Allow to set custom path for Lucee core

--- loader/src/main/java/lucee/loader/engine/CFMLEngineFactory.java
+++ loader/src/main/java/lucee/loader/engine/CFMLEngineFactory.java
@@ -332,6 +332,18 @@ public class CFMLEngineFactory extends CFMLEngineFactorySupport {
 						}
 					}
 
+					if (is == null) {
+						// check for custom path of Lucee core
+						String s = System.getProperty("lucee.core.path");
+						if (s != null) {
+							File dir = new File(s);
+							File[] files = dir.listFiles(new ExtensionFilter(new String[]{ coreExt }));
+							if (files.length > 0) {
+								is = new FileInputStream(files[0]);
+							}
+						}
+					}
+
 					if (is != null) {
 						os = new BufferedOutputStream(new FileOutputStream(isPack200 ? rcPack200 : rc));
 						copy(is, os);
