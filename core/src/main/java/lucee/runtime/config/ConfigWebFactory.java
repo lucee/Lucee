6efc7ff3852c1fc43a4ff295ba121736afa8fc72
michaeloffner <michaeloffner@users.noreply.github.com>
2023-02-17 15:46:58 +0100

    LDEV-4315 - avoid NPE

--- core/src/main/java/lucee/runtime/config/ConfigWebFactory.java
+++ core/src/main/java/lucee/runtime/config/ConfigWebFactory.java
@@ -2348,11 +2348,17 @@ public final class ConfigWebFactory extends ConfigFactory {
 
 				// first add the server drivers, so they can be overwritten
 				if (configServer != null) {
-					Iterator<ClassDefinition> it = configServer.getCacheDefinitions().values().iterator();
-					ClassDefinition cd;
-					while (it.hasNext()) {
-						cd = it.next();
-						map.put(cd.getClassName(), cd);
+					Map<String, ClassDefinition> cds = configServer.getCacheDefinitions();
+					if (cds != null) {
+						Collection<ClassDefinition> values = cds.values();
+						if (values != null) {
+							Iterator<ClassDefinition> it = values.iterator();
+							ClassDefinition cd;
+							while (it.hasNext()) {
+								cd = it.next();
+								map.put(cd.getClassName(), cd);
+							}
+						}
 					}
 				}
 				ClassDefinition cd;
