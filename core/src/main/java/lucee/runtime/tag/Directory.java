8f48120ee16392cd2904448dc82f22f682049291
michaeloffner <michaeloffner@users.noreply.github.com>
2023-11-28 08:36:56 +0100

    improve regression fix for directory listing

--- core/src/main/java/lucee/runtime/tag/Directory.java
+++ core/src/main/java/lucee/runtime/tag/Directory.java
@@ -476,6 +476,7 @@ public final class Directory extends TagImpl {
 			}
 			else {
 				// Query All
+				if (recurse && filter != null) filter = new OrResourceFilter(new ResourceFilter[] { filter, new DirectoryResourceFilter() });
 				_fillQueryAll(query, directory, filter, 0, hasMeta, recurse);
 			}
 		}
@@ -547,40 +548,9 @@ public final class Directory extends TagImpl {
 	}
 
 	private static int _fillQueryAll(Query query, Resource directory, ResourceFilter filter, int count, boolean hasMeta, boolean recurse) throws PageException, IOException {
-		if (!recurse && filter != null) {
-			Resource[] list = directory.listResources(filter);
+		// if (!recurse && filter != null) {
 
-			if (list == null || list.length == 0) return count;
-			String dir = directory.getCanonicalPath();
-			// fill data to query
-			// query.addRow(list.length);
-			boolean isDir;
-			boolean modeSupported = directory.getResourceProvider().isModeSupported();
-			for (int i = 0; i < list.length; i++) {
-				isDir = list[i].isDirectory();
-				query.addRow(1);
-				count++;
-				query.setAt(KeyConstants._name, count, list[i].getName());
-				query.setAt(KeyConstants._size, count, Double.valueOf(isDir ? 0 : list[i].length()));
-				query.setAt(KeyConstants._type, count, isDir ? "Dir" : "File");
-				if (modeSupported) {
-					query.setAt(MODE, count, new ModeObjectWrap(list[i]));
-				}
-				query.setAt(DATE_LAST_MODIFIED, count, new Date(list[i].lastModified()));
-				// TODO File Attributes are Windows only...
-				// this is slow as it fetches each the attributes one at a time
-				query.setAt(ATTRIBUTES, count, getFileAttribute(list[i], true));
-
-				if (hasMeta) {
-					query.setAt(META, count, ((ResourceMetaData) list[i]).getMetaData());
-				}
-
-				query.setAt(DIRECTORY, count, dir);
-			}
-			return count;
-		}
-
-		Resource[] list = directory.listResources();
+		Resource[] list = filter != null ? directory.listResources(filter) : directory.listResources();
 
 		if (list == null || list.length == 0) return count;
 		String dir = directory.getCanonicalPath();
@@ -590,29 +560,49 @@ public final class Directory extends TagImpl {
 		boolean modeSupported = directory.getResourceProvider().isModeSupported();
 		for (int i = 0; i < list.length; i++) {
 			isDir = list[i].isDirectory();
-			if (filter == null || filter.accept(list[i])) {
-				query.addRow(1);
-				count++;
-				query.setAt(KeyConstants._name, count, list[i].getName());
-				query.setAt(KeyConstants._size, count, Double.valueOf(isDir ? 0 : list[i].length()));
-				query.setAt(KeyConstants._type, count, isDir ? "Dir" : "File");
-				if (modeSupported) {
-					query.setAt(MODE, count, new ModeObjectWrap(list[i]));
-				}
-				query.setAt(DATE_LAST_MODIFIED, count, new Date(list[i].lastModified()));
-				// TODO File Attributes are Windows only...
-				// this is slow as it fetches each the attributes one at a time
-				query.setAt(ATTRIBUTES, count, getFileAttribute(list[i], true));
-
-				if (hasMeta) {
-					query.setAt(META, count, ((ResourceMetaData) list[i]).getMetaData());
-				}
-
-				query.setAt(DIRECTORY, count, dir);
+			query.addRow(1);
+			count++;
+			query.setAt(KeyConstants._name, count, list[i].getName());
+			query.setAt(KeyConstants._size, count, Double.valueOf(isDir ? 0 : list[i].length()));
+			query.setAt(KeyConstants._type, count, isDir ? "Dir" : "File");
+			if (modeSupported) {
+				query.setAt(MODE, count, new ModeObjectWrap(list[i]));
 			}
+			query.setAt(DATE_LAST_MODIFIED, count, new Date(list[i].lastModified()));
+			// TODO File Attributes are Windows only...
+			// this is slow as it fetches each the attributes one at a time
+			query.setAt(ATTRIBUTES, count, getFileAttribute(list[i], true));
+
+			if (hasMeta) {
+				query.setAt(META, count, ((ResourceMetaData) list[i]).getMetaData());
+			}
+
+			query.setAt(DIRECTORY, count, dir);
 			if (recurse && isDir) count = _fillQueryAll(query, list[i], filter, count, hasMeta, recurse);
 		}
 		return count;
+		// }
+
+		/*
+		 * Resource[] list = directory.listResources();
+		 * 
+		 * if (list == null || list.length == 0) return count; String dir = directory.getCanonicalPath(); //
+		 * fill data to query // query.addRow(list.length); boolean isDir; boolean modeSupported =
+		 * directory.getResourceProvider().isModeSupported(); for (int i = 0; i < list.length; i++) { isDir
+		 * = list[i].isDirectory(); if (filter == null || filter.accept(list[i])) { query.addRow(1);
+		 * count++; query.setAt(KeyConstants._name, count, list[i].getName());
+		 * query.setAt(KeyConstants._size, count, Double.valueOf(isDir ? 0 : list[i].length()));
+		 * query.setAt(KeyConstants._type, count, isDir ? "Dir" : "File"); if (modeSupported) {
+		 * query.setAt(MODE, count, new ModeObjectWrap(list[i])); } query.setAt(DATE_LAST_MODIFIED, count,
+		 * new Date(list[i].lastModified())); // TODO File Attributes are Windows only... // this is slow as
+		 * it fetches each the attributes one at a time query.setAt(ATTRIBUTES, count,
+		 * getFileAttribute(list[i], true));
+		 * 
+		 * if (hasMeta) { query.setAt(META, count, ((ResourceMetaData) list[i]).getMetaData()); }
+		 * 
+		 * query.setAt(DIRECTORY, count, dir); } if (recurse && isDir) count = _fillQueryAll(query, list[i],
+		 * filter, count, hasMeta, recurse); } return count;
+		 */
 	}
 
 	// this method only exists for performance reasion
