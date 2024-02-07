095b0f52fd2ab146e7b9f88bf3b584d9e753b5fe
michaeloffner <michaeloffner@users.noreply.github.com>
2024-02-07 16:04:33 +0100

    add FUTURE mark

--- core/src/main/java/lucee/runtime/engine/CFMLEngineImpl.java
+++ core/src/main/java/lucee/runtime/engine/CFMLEngineImpl.java
@@ -1490,7 +1490,7 @@ public final class CFMLEngineImpl implements CFMLEngine {
 	}
 
 	@Override
-	public Operation getOperatonUtil() {
+	public Operation getOperatonUtil() { // FUTURE rename to getOperationUtil()
 		return OperationImpl.getInstance();
 	}
 
