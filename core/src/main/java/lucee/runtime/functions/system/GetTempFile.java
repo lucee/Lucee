82b4eacc9dfdb43ba85e2de563de608a00d87dbf
michaeloffner <michaeloffner@users.noreply.github.com>
2024-02-12 19:23:52 +0100

    revert change myade by PR to the wrong branch (should be 6.1 instead of 6.0)

--- core/src/main/java/lucee/runtime/functions/system/GetTempFile.java
+++ core/src/main/java/lucee/runtime/functions/system/GetTempFile.java
@@ -22,7 +22,6 @@
 package lucee.runtime.functions.system;
 
 import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
 
 import lucee.commons.io.res.Resource;
 import lucee.commons.io.res.util.ResourceUtil;
@@ -30,12 +29,10 @@ import lucee.commons.lang.StringUtil;
 import lucee.runtime.PageContext;
 import lucee.runtime.exp.ExpressionException;
 import lucee.runtime.exp.PageException;
-import lucee.runtime.exp.PageExceptionImpl;
 import lucee.runtime.ext.function.Function;
 import lucee.runtime.op.Caster;
 
 public final class GetTempFile implements Function {
-
 	public static String call(PageContext pc, String strDir, String prefix) throws PageException {
 		return call(pc, strDir, prefix, ".tmp");
 	}
@@ -44,38 +41,20 @@ public final class GetTempFile implements Function {
 		Resource dir = ResourceUtil.toResourceExisting(pc, strDir);
 		pc.getConfig().getSecurityManager().checkFileLocation(dir);
 		if (!dir.isDirectory()) throw new ExpressionException("[" + strDir + "] is not a directory");
+		int count = 1;
 		Resource file;
-
-		final int MAX_RETRY = 60;
-		boolean fileCreated = false;
-		String randomPart = getRandomChar();
-		if (extension.trim().isEmpty() == true) { extension = ".tmp"; }
-		if (extension.charAt(0) != '.') { extension = "." + extension; }
-		do {
-			file = dir.getRealResource(prefix + pc.getId() + randomPart + extension);
-			fileCreated = file.createNewFile();
-			if (fileCreated) {
-				try {
-					return file.getCanonicalPath();
-				} catch (IOException e){
-					// File was created, yet, we have an exception this is probably pretty bad
-					throw Caster.toPageException(e);
-				}
-			}
-			randomPart += getRandomChar();
-		} while (randomPart.length() < MAX_RETRY);
-		throw new ExpressionException(
-			// Most FS will fail before reaching 64 chars length, so we might fail faster,
-			// However, if we reach this pooint, we tried 62^60 possible combinations, 
-			// The algorithm can't ensure perfect distribution, but collisions will be quite rare
-			"Unable to create temporary file in [" + strDir + "] after " + MAX_RETRY + " tries", 
-			"IOException"
-		);
-	}
-
-	private static String getRandomChar() {
-		String charWhitelist = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int randomInt = ThreadLocalRandom.current().nextInt(charWhitelist.length());
-		return "" + charWhitelist.charAt(randomInt);
+		if (StringUtil.isEmpty(extension, true)) extension = ".tmp";
+		if (extension.charAt(0) != '.') extension = "." + extension;
+		while ((file = dir.getRealResource(prefix + pc.getId() + count + extension)).exists()) {
+			count++;
+		}
+		try {
+			file.createFile(false);
+			// file.createNewFile();
+			return file.getCanonicalPath();
+		}
+		catch (IOException e) {
+			throw Caster.toPageException(e);
+		}
 	}
 }
