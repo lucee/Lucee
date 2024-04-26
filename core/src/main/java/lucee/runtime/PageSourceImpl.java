bbdaff55bc17aed6ad3fa16622dfc2fdd4ee45f0
michaeloffner <michaeloffner@users.noreply.github.com>
2024-04-26 18:26:40 +0200

    LDEV-4824 - sync value creation of shared instances

--- core/src/main/java/lucee/runtime/PageSourceImpl.java
+++ core/src/main/java/lucee/runtime/PageSourceImpl.java
@@ -297,21 +297,22 @@ public final class PageSourceImpl implements PageSource {
 	private Page loadArchive(Page page, Page defaultValue) {
 		if (!mapping.hasArchive()) return defaultValue;
 		if (page != null && page.getLoadType() == LOAD_ARCHIVE) return page;
-		if (!getArchiveClass().isFile()) {
-			return defaultValue;
+		synchronized (this) {
+			if (!getArchiveClass().isFile()) {
+				return defaultValue;
+			}
+			try {
+				Class clazz = mapping.getArchiveClass(getClassName());
+				page = newInstance(clazz);
+				page.setPageSource(this);
+				page.setLoadType(LOAD_ARCHIVE);
+				pcn.set(page);
+				return page;
+			}
+			catch (Exception e) {
+				return defaultValue;
+			}
 		}
-		try {
-			Class clazz = mapping.getArchiveClass(getClassName());
-			page = newInstance(clazz);
-			page.setPageSource(this);
-			page.setLoadType(LOAD_ARCHIVE);
-			pcn.set(page);
-			return page;
-		}
-		catch (Exception e) {
-			return defaultValue;
-		}
-
 	}
 
 	/**
