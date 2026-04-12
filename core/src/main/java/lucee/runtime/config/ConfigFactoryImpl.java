856b6af6b10c275a71fd524ab634cb7107c13a91
Michael Offner <michael@lucee.org>
2026-04-12 18:18:39 +0200

    Merge commit 'b4bd4c7b8a35bdb9d0453210eade9ed8a7fd9177' into 7.2
    # Conflicts:
    #	core/src/main/java/lucee/runtime/config/ConfigFactoryImpl.java
    #	loader/build.xml
    #	loader/pom.xml

--- core/src/main/java/lucee/runtime/config/ConfigFactoryImpl.java
+++ core/src/main/java/lucee/runtime/config/ConfigFactoryImpl.java
@@ -144,16 +144,6 @@ public final class ConfigFactoryImpl extends ConfigFactory {
 
 		((GatewayEngineImpl) configWeb.getGatewayEngine()).autoStart();
 
-		// invoke config listener if set
-		try {
-			ConfigListener listener = configServer.getConfigListener();
-			if (listener != null) listener.onLoadWebContext(configServer, configWeb);
-		}
-		catch (Throwable t) {
-			ExceptionUtil.rethrowIfNecessary(t);
-			log(configServer, t);
-		}
-
 		log(configServer, Log.LEVEL_INFO,
 				"\n===================================================================\n" + "WEB CONTEXT (" + createLabel(configServer, servletConfig) + ")\n"
 						+ "-------------------------------------------------------------------\n" + "- config:" + configDir + "\n" + "- webroot:"
