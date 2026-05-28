e43939dddeacde099019cc47cb668463069ded04
Zac Spitzer <zac.spitzer@gmail.com>
2026-05-28 18:59:24 +0200

    LDEV-6348 shutdown InspectAutoRefresh thread on engine swap
    (cherry picked from commit 796368f7d0fe415253fce0c29da5dcba5353031f)

--- core/src/main/java/lucee/runtime/config/ConfigAdmin.java
+++ core/src/main/java/lucee/runtime/config/ConfigAdmin.java
@@ -3705,7 +3705,7 @@ public final class ConfigAdmin {
 
 		synchronized (factory) {
 			try {
-				cleanUp(factory);
+				cleanUp(factory, cs);
 				factory.update(cs.getPassword(), cs.getIdentification());
 			}
 			catch (Exception e) {
@@ -3755,7 +3755,7 @@ public final class ConfigAdmin {
 
 		try {
 			CFMLEngineFactory factory = cs.getEngine().getCFMLEngineFactory();
-			cleanUp(factory);
+			cleanUp(factory, cs);
 			// do we have the core file?
 			final File patchDir = factory.getPatchDirectory();
 			File localPath = new File(version.toString() + ".lco");
@@ -3805,7 +3805,7 @@ public final class ConfigAdmin {
 
 		try {
 			CFMLEngineFactory factory = cs.getEngine().getCFMLEngineFactory();
-			cleanUp(factory);
+			cleanUp(factory, cs);
 			// do we have the core file?
 			final File patchDir = factory.getPatchDirectory();
 			File localPath = new File(version.toString() + ".lco");
@@ -3847,7 +3847,8 @@ public final class ConfigAdmin {
 		}
 	}
 
-	private void cleanUp(CFMLEngineFactory factory) throws IOException {
+	private void cleanUp(CFMLEngineFactory factory, ConfigServerImpl cs) throws IOException {
+		cs.shutdown();
 		final File patchDir = factory.getPatchDirectory();
 		final File[] patches = patchDir.listFiles(new ExtensionFilter(new String[] { ".lco" }));
 		for (final File patch: patches) {
@@ -3980,7 +3981,7 @@ public final class ConfigAdmin {
 
 		synchronized (factory) {
 			try {
-				cleanUp(factory);
+				cleanUp(factory, cs);
 				factory.restart(cs.getPassword());
 			}
 			catch (Exception e) {
@@ -3994,6 +3995,8 @@ public final class ConfigAdmin {
 
 		synchronized (factory) {
 			try {
+				cs.shutdown();
+
 				// Get a lookup instance for accessing private methods
 				MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(factory.getClass(), MethodHandles.lookup());
 
