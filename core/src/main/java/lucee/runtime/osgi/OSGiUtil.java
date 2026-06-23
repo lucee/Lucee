a464029d5752cb7eafcc1e5fc96364cfa86667c5
Michael Offner <michael@lucee.org>
2026-06-23 14:59:11 +0200

    add jcifs package bundle mapping

--- core/src/main/java/lucee/runtime/osgi/OSGiUtil.java
+++ core/src/main/java/lucee/runtime/osgi/OSGiUtil.java
@@ -178,6 +178,7 @@ public final class OSGiUtil {
 		// extensions
 		packageBundleMapping.put("com.sun.jna", "com.sun.jna");
 		// packageBundleMapping.put("org.apache.commons.lang", "org.apache.commons.lang");
+		packageBundleMapping.put("jcifs.smb", "jcifs");
 	}
 
 	/**
